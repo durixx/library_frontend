c9809376ec4a252c99c0b3fb1c29ed33565a8b0c
serox2255 <marian22durco@gmail.com>
2022-11-01 12:39:40 +0100

    Table of all libraries #5 add home table, fixed css/html

---
+++ src/app/library-design/library/home/library-home.component.ts
@@ -0,0 +1,66 @@
+import {Component,} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {Library} from '../creation-form/library.model';
+import {LibraryControlService} from '../library-control.service';
+
+
+
+
+@Component({
+  selector: 'dur-app-home',
+  templateUrl: './library-home.component.html',
+  styleUrls: ['./library-home.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+
+export class LibraryHomeComponent {
+
+  displayedColumns: TableColumn[] = [
+    {field: 'id', label: 'Id', visible: false},
+    {field: 'nameOfLibrary', label: 'Name', visible: true},
+];
+
+  dataSource: Library[] = [];
+  columnsToDisplayWithExpand = [...this.displayedColumns.map(col => col.field), 'expand'];
+  expandedElement: Library | null;
+
+  constructor(private http: HttpClient, public control: LibraryControlService) {
+  }
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:4200/api/library/all')
+      .subscribe((dataSource: Library[])  => {
+        console.log('library', dataSource)
+        this.dataSource = dataSource;
+      });
+  }
+
+  public getVisibleColumns(): TableColumn[] {
+    return this.displayedColumns.filter(col => col.visible);
+  }
+
+  changePageToNewLib() {
+    this.control.changeModeToNewLibrary();
+  }
+}
+
+export class TableColumn {
+  field: string;
+  label: string;
+  visible: boolean;
+}
+
+
+
+
+
+
+
